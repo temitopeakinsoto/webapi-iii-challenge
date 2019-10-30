de61eab9ccaf9fbe61d28fd0a30935478d70121d
Temitope Akinsoto <takinsoto@gmail.com>
2019-10-30 14:17:37 +0100

    Import cors, helmet third-party middlewares

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
+const server = require('./server.js');
 
-app.use(cors());
-app.use(express.json());
 
-app.use('/', (req, res) => {
-    res.status(200).json({
-        message: "Hello, World!"
-    })
-})
-
-app.listen(process.env.PORT || 5000, () => {
+server.listen(process.env.PORT || 5000, () => {
   console.log("server running on port " + (process.env.PORT || 5000));
 });
--- server.js
+++ server.js
@@ -1,6 +1,17 @@
-const express = 'express';
-
+const express = require('express');
+const helmet = require('helmet');
+const cors = require("cors");
 const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+
+
+
+
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
