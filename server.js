b0fe210797b4823a0d70fbe0234c259c66d07c18
Temitope Akinsoto <takinsoto@gmail.com>
2019-10-30 17:12:58 +0100

    Create and implement logger middleware
    In this commit, i created the logger() middleware which logs the request method, the request url, and timestamp to the console

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const userRouter = require('./users/userRouter.js');
 
-
 const helmet = require('helmet');
 const cors = require("cors");
 const server = express();
@@ -9,24 +8,18 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
-
+server.use((req, res, next) => {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.originalUrl}`
+  );
+  next();
+});
 
 server.use('/api/users', userRouter);
-
-
-
-
-
-
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
-//custom middleware
-
-function logger(req, res, next) {
-
-};
 
 module.exports = server;
