35113d82b49556d9689ffa934b76eff33165a6ba
Temitope Akinsoto <takinsoto@gmail.com>
2019-10-30 15:09:06 +0100

    Create a validateUserId middleware
    In this commit, validateUserId middleware was created. this middleware will then be used to validate the id of a user making a get or post request to a particular route

--- users/userRouter.js
+++ users/userRouter.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
+const Users = require('./users/userDb.js');
+
 router.post('/', (req, res) => {
 
 });
@@ -33,6 +35,20 @@ router.put('/:id', (req, res) => {
 //custom middleware
 
 function validateUserId(req, res, next) {
+    users.getById(req.params.id)
+    .then(user => {
+      if (user) {
+        req.user = user;
+        next()
+      } else {
+        res.status(404).json({ message: 'User id does not correspond with an actual User' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: 'Something terrible happend while checking user id: ' + error.message,
+      });
+    });
 
 };
 
