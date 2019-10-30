8f295bce0b13080a8deb3134d09eef98da6600dd
Temitope Akinsoto <takinsoto@gmail.com>
2019-10-30 13:49:43 +0100

    Create a barebone api server file
    In this commit, I created a barebone api server which runs on port 5000 and returns an  'Hello, world greeting' for the / endpoint

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code away!
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/', (req, res) => {
+    res.status(200).json({
+        message: "Hello, World!"
+    })
+})
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("server running on port " + (process.env.PORT || 5000));
+});
