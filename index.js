5e09322f0e0250d89ce01267fa92ecef50461a42
Temitope Akinsoto <takinsoto@gmail.com>
2019-10-31 15:29:01 +0100

    setup PORT number within .env file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const server = require('./server.js');
+const port = process.env.PORT
 
 
-server.listen(process.env.PORT || 5000, () => {
-  console.log("server running on port " + (process.env.PORT || 5000));
+server.listen(port, () => {
+  console.log("server running on port " + port);
 });
