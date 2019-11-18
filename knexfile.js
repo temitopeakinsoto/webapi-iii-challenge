b0fb7ec055d81df96a1dbd9faa832a4996c446e6
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-18 08:51:52 +0100

    Add npm package pg, set up dev environment within knexfile

--- knexfile.js
+++ knexfile.js
@@ -18,4 +18,15 @@ module.exports = {
       directory: './data/seeds',
     },
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations',
+      tableName: 'knex_migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  }
 };
